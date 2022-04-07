dc1a44cf45eab2896ff191084618e94ea106c202
reymonr <reymonr@unklab.ac.id>
2022-04-07 10:01:47 +0800

    edit info

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn CI-CD with GitHub Actions
         </a>
       </header>
     </div>
