debd6bce5d70a775742138e2ccf01caceab45ccb
reymonr <reymonr@users.noreply.github.com>
2022-04-07 10:30:30 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn CI-CD with GitHub Actions
+          Learn GitHub Actions
         </a>
       </header>
     </div>
