61e68cab4ffee9198d7b949bddb374480b9712fc
reymonr <reymonr@unklab.ac.id>
2022-04-11 14:39:41 +0800

    update test file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          CI/CD with GitHub Actions
+          Learn GitHub Actions
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,10 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn github actions/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+// solution:
+https://stackoverflow.com/questions/71685441/react-testing-library-gives-console-error-for-reactdom-render-in-react-18
