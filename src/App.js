0e96323797f62f792731300476a7e3cbb3c1b2b9
reymonr <reymonr@unklab.ac.id>
2022-04-11 15:01:41 +0800

    update tagline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          GitHub Actions Example
+          CI/CD with GitHub Actions: An Example
         </p>
         <a
           className="App-link"
