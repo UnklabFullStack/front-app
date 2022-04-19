a6ca59fe4a631398150bd54a95100eb35691fc40
reymonr <reymonr@unklab.ac.id>
2022-04-19 09:02:04 +0800

    change tagline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CI/CD with GitHub Actions: Full Stack Web Development
+          Continuous Integration/Continuous Deployment
         </p>
         <a
           className="App-link"
